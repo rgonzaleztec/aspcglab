993efc1065075c487603123b7599c64f45c96443
Josué Chaves Araya <d4n11083@estudiantec.cr>
2023-06-20 11:24:08 -0600

    add: Information and button to CanoNegro1

--- ASPAumentado/Assets/Scripts/ChangeSeasonBtn.cs
+++ ASPAumentado/Assets/Scripts/ChangeSeasonBtn.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ChangeSeasonBtn : MonoBehaviour
 {
 
 
     public GameObject[] animalPins;
+    public GameObject button;
+
+
+    private void OnMouseDown()
+    {
+
+        Vector3 position = this.GetComponent<Transform>().position;
+
+        this.button.GetComponent<Transform>().position = new Vector3(position.x, this.button.GetComponent<Transform>().position.y - 0.6f,position.z);
+
+    }
     // Start is called before the first frame update
     private void OnMouseUpAsButton()
     {
         this.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
         changePinSeasons();
+
+        Vector3 position = this.GetComponent<Transform>().position;
+        this.button.GetComponent<Transform>().position = new Vector3(position.x, this.button.GetComponent<Transform>().position.y + 0.6f, position.z);
     }
+
+
+
 
     private void changePinSeasons()
     {
