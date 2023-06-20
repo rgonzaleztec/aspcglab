b6fb5d76a50c8acc87650ac76512e5afe51699b1
Josué Chaves Araya <d4n11083@estudiantec.cr>
2023-06-20 12:02:42 -0600

    fix: Button now changes label text CN1

--- ASPAumentado/Assets/Scripts/ChangeSeasonBtn.cs
+++ ASPAumentado/Assets/Scripts/ChangeSeasonBtn.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using UnityEngine.UIElements;
 
 public class ChangeSeasonBtn : MonoBehaviour
@@ -9,14 +12,22 @@ public class ChangeSeasonBtn : MonoBehaviour
 
     public GameObject[] animalPins;
     public GameObject button;
+    public TextMeshProUGUI informationLabelText;
+    
+    private bool isDrySeason = false;
 
+    private void Start()
+    {
+        changeInformationText();
+
+    }
 
     private void OnMouseDown()
     {
 
         Vector3 position = this.GetComponent<Transform>().position;
 
-        this.button.GetComponent<Transform>().position = new Vector3(position.x, this.button.GetComponent<Transform>().position.y - 0.6f,position.z);
+        this.button.GetComponent<Transform>().position = new Vector3(position.x, position.y - 0.01f,position.z);
 
     }
     // Start is called before the first frame update
@@ -26,14 +37,36 @@ public class ChangeSeasonBtn : MonoBehaviour
         changePinSeasons();
 
         Vector3 position = this.GetComponent<Transform>().position;
-        this.button.GetComponent<Transform>().position = new Vector3(position.x, this.button.GetComponent<Transform>().position.y + 0.6f, position.z);
+        this.button.GetComponent<Transform>().position = new Vector3(position.x, position.y + 0.01f, position.z);
+        changeInformationText();
     }
 
+    private void changeInformationText()
+    {
+
+        if (this.isDrySeason)
+        {
+            string info = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_RainySeasonInformationLabel_Text");
+            this.informationLabelText.text = info;
+
+            this.isDrySeason= false;
+        }
+        else
+        {
+            string info  = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_DrySeasonInformationLabel_Text");
+            this.informationLabelText.text = info;
+            this.isDrySeason = true;
+        }
+
+    }
+   
 
 
 
     private void changePinSeasons()
     {
+
+
         foreach(GameObject item in animalPins)
         {
             Debug.Log(item.name);
