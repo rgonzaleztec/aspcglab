b4744fde62ddfe3b4afb1940a63bcdda01402b53
Josué Chaves <d4n11083@estudiantec.cr>
2024-10-19 19:25:07 -0600

    Redesign CN 1

--- ASPAumentado/Assets/Scripts/ChangeSeasonBtn.cs
+++ ASPAumentado/Assets/Scripts/ChangeSeasonBtn.cs
@@ -4,64 +4,74 @@ using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class ChangeSeasonBtn : MonoBehaviour
 {
 
 
     public GameObject[] animalPins;
-    public GameObject button;
-    public TextMeshProUGUI informationLabelText;
-    
-    private bool isDrySeason = false;
+
+    private bool isDrySeason = false; //True = Dry, False = Rainy  
+    [SerializeField]
+    private Image weatherImage;
+    [SerializeField]
+    private Sprite DrySeasonImage;
+    [SerializeField]
+    private Sprite RainySeasonImage;
+
+    [SerializeField]
+    private TextMeshProUGUI seasonLabel;
+
+    [SerializeField]
+    private TextMeshProUGUI startMonthLabel;
+
+    [SerializeField]
+    private TextMeshProUGUI finishMonthLabel;
+
+
 
     private void Start()
     {
         changeInformationText();
 
-    }
+    }  
 
-    private void OnMouseDown()
-    {
-
-        Vector3 position = this.GetComponent<Transform>().position;
-
-        this.button.GetComponent<Transform>().position = new Vector3(position.x, position.y - 0.01f,position.z);
-
-    }
-    // Start is called before the first frame update
-    private void OnMouseUpAsButton()
-    {
-        this.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-        changePinSeasons();
-
-        Vector3 position = this.GetComponent<Transform>().position;
-        this.button.GetComponent<Transform>().position = new Vector3(position.x, position.y + 0.01f, position.z);
-        changeInformationText();
-    }
-
-    private void changeInformationText()
+    public void changeInformationText()
     {
 
         if (this.isDrySeason)
         {
-            string info = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_RainySeasonInformationLabel_Text");
-            this.informationLabelText.text = info;
 
-            this.isDrySeason= false;
+
+            changePinSeasons();
+            weatherImage.sprite = DrySeasonImage;
+            seasonLabel.text = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_DrySeasonInformationLabel_Text");
+            startMonthLabel.text = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_DrySeasonStartMonth_Text");
+            finishMonthLabel.text = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_DrySeasonFinishMonth_Text");
+
+
+           
+            
+
+            this.isDrySeason = false;
         }
         else
         {
-            string info  = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_DrySeasonInformationLabel_Text");
-            this.informationLabelText.text = info;
+
+            changePinSeasons();
+            weatherImage.sprite = RainySeasonImage;
+            seasonLabel.text = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_RainySeasonInformationLabel_Text"); ;
+            startMonthLabel.text = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_RainySeasonStartMonth_Text");
+            finishMonthLabel.text = LocalizationSettings.StringDatabase.GetLocalizedString("RN_CANONEGRO_TEXT", "RCN1_RainySeasonFinishMonth_Text");
+
+
             this.isDrySeason = true;
+
         }
 
     }
    
-
-
 
     private void changePinSeasons()
     {
